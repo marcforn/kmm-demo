539d8213fb3ebc5119a1f3846ba70778bbcdefb8
mforn <mforn@zinio.com>
2020-09-23 19:13:57 +0200

    Update expect/actual

--- shared/src/iosMain/kotlin/com/mforn/kmmdemo/shared/configuration/Platform.kt
+++ shared/src/iosMain/kotlin/com/mforn/kmmdemo/shared/configuration/Platform.kt
@@ -1,5 +1,6 @@
 package com.mforn.kmmdemo.shared.configuration
 
+// TODO mforn: 23/09/20 review
 actual class Platform actual constructor() {
-    actual val platform: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    actual val platform: String = "3434543543543"
 }
