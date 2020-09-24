6b379424bb13ba14785ecf15fe5c32dda193d434
mforn <mforn@zinio.com>
2020-09-24 20:58:38 +0200

    Fix ios app (Test8)

--- shared/src/commonMain/kotlin/com/mforn/kmmdemo/shared/SdkManager.kt
+++ shared/src/commonMain/kotlin/com/mforn/kmmdemo/shared/SdkManager.kt
@@ -2,7 +2,7 @@ package com.mforn.kmmdemo.shared
 
 import com.mforn.kmmdemo.shared.domain.interactor.RocketInteractorImpl
 
-object SdkManager {
+class SdkManager {
 
     val launches = RocketInteractorImpl()
 }
